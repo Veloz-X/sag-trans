c4dc3909bffdf4beb245b1cccf7d9ed081575fbf
Veloz-X <viralveloz@gmail.com>
2020-10-24 03:11:22 -0500

    v0.34.b
    v0.34.b

--- a/a_net.php
+++ a/a_net.php
@@ -34,8 +34,8 @@ if(mysqli_num_rows($result) > 0)
 
 
 				<td><font  size=2>
-				<strong>Doc. # </strong>'.$row["documento"].'&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;<strong>Estado: </strong><small class="text-muted">
-				&nbsp; &nbsp;Activo
+				<strong>Doc. # </strong>'.$row["documento"].'&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;<strong>Estado: </strong><small class="text-muted">
+				&nbsp; Activo &nbsp;
 				<div class="spinner-grow spinner-grow-sm '.$row["estado"].'" role="status">
 				  <span class="sr-only">Loading...</span>
 				</div>
