ccfc70c21db455142df7b3cd7cce76b6a4c2ff1d
Veloz-X <viralveloz@gmail.com>
2020-10-24 02:35:21 -0500

    v0.24.b
    v0.24.b

--- admin/Nueva carpeta/config.php
+++ admin/Nueva carpeta/config.php
@@ -1,14 +1,13 @@
-<?php
+<?php 
 
-$server = 'localhost:3308';
-$username = 'root';
-$password = '';
-$database = 'trasns1_sag';
 
-try {
-  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
-} catch (PDOException $e) {
-  die('Connection Failed: ' . $e->getMessage());
+$conn= new mysqli("localhost","storcdnu_trans_sag","Siselacontra1","storcdnu_trans_sag");
+
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+	exit;
 }
+$conn->set_charset("utf8");
+
 
 ?>
