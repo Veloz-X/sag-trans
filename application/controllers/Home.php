4b8f197ae4336998c776d137d00f2d29e5355684
Veloz-X <viralveloz@gmail.com>
2020-10-24 01:24:34 -0500

    V7 erro helpers
    elimine funcion constructo y lo puse en f index

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,14 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper(array('getmenu','url'));
-	}
+
 
 	public function index()
 	{
+		$this->load->helper(array('getmenu','url'));
 		$data['menu'] = menu_home();
 		$this->load->view('home');
 	}
