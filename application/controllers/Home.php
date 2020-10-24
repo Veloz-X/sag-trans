d4709421a4c5cc5ed6ee7ed99456f411426468b6
Veloz-X <viralveloz@gmail.com>
2020-10-24 01:15:47 -0500

    v5 error helper
    Cambie array autoload

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->helper(array('getmenu_helper','url'));
+		$this->load->helper(array('getmenu','url'));
 	}
 
 	public function index()
