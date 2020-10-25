c29e16565fcd85469191fdae1eede37bcdc49533
Veloz-X <viralveloz@gmail.com>
2020-10-25 02:55:25 -0500

    v0.53.b
    v0.53.b

--- a/details.php
+++ a/details.php
@@ -1,6 +1,16 @@
 <?php
 include '../bd/config.php';
-include '../a/action.php'
+include '../a/action.php';
+$id=$_GET['details'];
+$query="SELECT * FROM $table WHERE id=?";
+$stmt=$conn->prepare($query);
+$stmt->bind_param("i",$id);
+$stmt->execute();
+$result=$stmt->get_result();
+$row=$result->fetch_assoc();
+
+$vid=$row['id'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
