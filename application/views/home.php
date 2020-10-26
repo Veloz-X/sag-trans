e2cc9f7487ad907bdb2c7876f4262aed54ff4e23
Veloz-X <viralveloz@gmail.com>
2020-10-25 19:28:12 -0500

    v0.70.b
    v0.70.b

--- application/views/home.php
+++ application/views/home.php
@@ -242,14 +242,11 @@ $menu = array(
           <div class="row row-cols-1 row-cols-md-5 p-1">
             <!-- INICIO ACTUALIZACION V2 -->
             <div class="card text-white p-2">
-              <img src="https://seagm-media.seagmcdn.com/special_deals/card/595.jpg" class="card-img " width="314px" height="120px" alt="...">
-              <div class="card-img-overlay">
+              <img src="https://seagm-media.seagmcdn.com/special_deals/card/595.jpg" class="card-img " width="314px" height="120px" >
+              <div class="card">
               
-              <img src="https://elrincondenetflix.com/wp-content/uploads/2020/04/nuevo-logo-netflix_original.jpg" class="card-img " width="72px" height="72px" >
-             
+              <img src="https://elrincondenetflix.com/wp-content/uploads/2020/04/nuevo-logo-netflix_original.jpg" width="72px" height="72px" >
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
               </div>
             </div>
             <!-- FIN ACTUALIZACION V2 -->
