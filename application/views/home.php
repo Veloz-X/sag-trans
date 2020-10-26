6f91a97c119c8a392dfb6da155b2f8cf5c976c99
Veloz-X <viralveloz@gmail.com>
2020-10-25 19:01:23 -0500

    v0.65.b
    v0.65.b

--- application/views/home.php
+++ application/views/home.php
@@ -242,7 +242,8 @@ $menu = array(
           <div class="row row-cols-1 row-cols-md-5 p-1">
             <!-- INICIO ACTUALIZACION V2 -->
             <div class="card bg-dark text-white">
-              <img src="..." class="card-img" alt="...">
+              <img src="https://seagm-media.seagmcdn.com/special_deals/card/595.jpg" class="card-img" width="314px"
+         height="120px" alt="...">
               <div class="card-img-overlay">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
