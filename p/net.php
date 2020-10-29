ff6f0086e745e8773aeee74022d66f3c80134274
Veloz-X <viralveloz@gmail.com>
2020-10-29 15:20:42 -0500

    v.1.7.b
    v.1.7.b

--- p/net.php
+++ p/net.php
@@ -8,7 +8,7 @@ include '../a/action.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Free Fire</title>
+	<title>Netflix</title>
 	<link rel="stylesheet" href="css/free.css">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
