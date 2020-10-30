78bb4a07ccbad4a58e84047742fb5b964799e6c5
Veloz-X <viralveloz@gmail.com>
2020-10-29 23:43:31 -0500

    v.1.16.b
    null

--- a/a_google.php
+++ a/a_google.php
@@ -37,7 +37,7 @@ if(mysqli_num_rows($result) > 0)
 				<br>
 				<strong>Estado: </strong><a class="badge badge-success p-1 text-light">Pagado</a>
 				<br>
-				Google Play Gift Card <strong>'.$row["cantidad"].'</strong> USD US <img src="https://media.istockphoto.com/vectors/circle-flag-united-states-of-america-usa-flag-icon-vector-vector-id862664854?b=1&k=6&m=862664854&s=170x170&h=W_E5dhqpVzc5omi08b99O3JCfXKmbEbxY4jJIQi6YuE=" width="20">
+				Google Play Gift Card <strong>'.$row["cantidad"].'</strong> USD US<img src="https://media.istockphoto.com/vectors/circle-flag-united-states-of-america-usa-flag-icon-vector-vector-id862664854?b=1&k=6&m=862664854&s=170x170&h=W_E5dhqpVzc5omi08b99O3JCfXKmbEbxY4jJIQi6YuE=" width="35">
 				<br>
 				<img src="https://seagm-media.seagmcdn.com/icon_400/225.jpg" width="20">: &nbsp;'.$row["c"].'
 				<br>
