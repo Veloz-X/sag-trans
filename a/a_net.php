c3af608e6af303ea35c81449fd4e732b2ee84c50
Veloz-X <viralveloz@gmail.com>
2020-11-05 20:46:44 -0500

    v.1.51.b
    v.1.51.b

--- a/a_net.php
+++ a/a_net.php
@@ -79,14 +79,14 @@ else
 	  <!-- REDES SOCIALES -->
 	  <tr>
 	  <td class="m_3731599029898677976content-block" style="text-align:center;width:100%" colspan="2">
-		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#a3a3a3;">Horario de Servicio de Activación</span><br>
+		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#000000;">Horario de Servicio de Activación</span><br>
 		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#a3a3a3;">Lunes a Viernes</span><br>
 		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#a3a3a3;">Demora de 1 hora Hasta 12 Horas</span>
 		  </td>
 	  </tr>
 	  <tr>
 		<td class="m_3731599029898677976content-block" style="text-align:center;width:100%" colspan="2">
-		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#a3a3a3;">Estados de Netflix</span><br>
+		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#000000;">Estados de Netflix</span><br>
 		  <span style="font-family: Arial, Helvetica, sans-serif; font-size: small; color:#a3a3a3;">En espera para Activación  <div class="spinner-grow spinner-grow-sm" role="status">
 		  <span class="sr-only">Loading...</span>
 		</div></span><br>
