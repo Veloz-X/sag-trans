a718fd0df502c691d014bc5e8a086e4c3b45a8ad
Veloz-X <viralveloz@gmail.com>
2020-11-18 18:30:17 -0500

    Prueba
    Prueba 1

--- application/controllers/Buscar.php
+++ application/controllers/Buscar.php
@@ -4,8 +4,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Buscar extends CI_Controller {
 
     function index()
-    {
-        $this->load->view('buscar');
-    }
+	{
+		$this->load->view('buscar');
+	}
 
+	function fetch()
+	{
+		$output = '';
+		$query = '';
+		$this->load->model('buscar_model');
+		if($this->input->post('query'))
+		{
+			$query = $this->input->post('query');
+		}
+		$data = $this->ajaxsearch_model->fetch_data($query);
+		$output .= '
+		<div class="table-responsive">
+					<table class="table table-bordered table-striped">
+						<tr>
+							<th>Customer Name</th>
+							<th>Address</th>
+							<th>City</th>
+							<th>Postal Code</th>
+							<th>Country</th>
+						</tr>
+		';
+		if($data->num_rows() > 0)
+		{
+			foreach($data->result() as $row)
+			{
+				$output .= '
+						<tr>
+							<td>'.$row->documento.'</td>
+							<td>'.$row->documento.'</td>
+							<td>'.$row->documento.'</td>
+							<td>'.$row->documento.'</td>
+							<td>'.$row->documento.'</td>
+						</tr>
+				';
+			}
+		}
+		else
+		{
+			$output .= '<tr>
+							<td colspan="5">No Data Found</td>
+						</tr>';
+		}
+		$output .= '</table>';
+		echo $output;
+	}
+	
 }
---
+++ application/models/Buscar_model.php
@@ -0,0 +1,20 @@
+<?php
+class Buscar_model extends CI_Model
+{
+	function fetch_data($query)
+	{
+		$this->db->select("*");
+		$this->db->from("t_net");
+		if($query != '')
+		{
+			$this->db->like('documento', $query);
+			$this->db->or_like('documento', $query);
+			$this->db->or_like('documento', $query);
+			$this->db->or_like('documento', $query);
+			$this->db->or_like('documento', $query);
+		}
+		$this->db->order_by('CustomerID', 'DESC');
+		return $this->db->get();
+	}
+}
+?>
