ca2c6f28f86bdeeb013431d5eb915cbd9f64a911
Veloz-X <viralveloz@gmail.com>
2020-11-18 18:37:28 -0500

    Prueba 4
    Prueba 4

--- application/models/Buscar_model.php
+++ application/models/Buscar_model.php
@@ -8,10 +8,10 @@ class Buscar_model extends CI_Model
 		if($query != '')
 		{
 			$this->db->like('documento', $query);
-			$this->db->or_like('documento', $query);
-			$this->db->or_like('documento', $query);
-			$this->db->or_like('documento', $query);
-			$this->db->or_like('documento', $query);
+			$this->db->or_like('fecha', $query);
+			$this->db->or_like('fecha_i', $query);
+			$this->db->or_like('fecha_f', $query);
+			$this->db->or_like('correo', $query);
 		}
 		$this->db->order_by('CustomerID', 'DESC');
 		return $this->db->get();
