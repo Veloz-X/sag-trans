32adfccab46075a2c4b1d40245f00e45ea08e1ea
Veloz-X <viralveloz@gmail.com>
2020-11-19 16:54:35 -0500

    null
    null

--- application/views/home.php
+++ application/views/home.php
@@ -278,10 +278,12 @@ $giftcard = array(
                         <div class="col-auto shadow">
                           <img class="" style="border-radius: 8px; float:left;" width="72" height="72" src="<?= $item['icono'] ?>">
                         </div>
+                        <h5 style="float:left; ">&nbsp;</h5>
                         <div class="col mr-2">
-                          <div class="h5 mb-0 font-weight-bold text-gray-800">&nbsp;<?= $item['title'] ?></div>
+                        
+                          <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $item['title'] ?></div>
                           <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                          &nbsp;Revisa tu Orden</div>
+                          Revisa tu Orden</div>
 
                         </div>
 
