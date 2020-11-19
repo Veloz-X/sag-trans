6fda5639a07d124162b1a17884f83760707361ed
Veloz-X <viralveloz@gmail.com>
2020-11-19 16:46:22 -0500

    null
    null

--- application/views/home.php
+++ application/views/home.php
@@ -278,7 +278,7 @@ $giftcard = array(
                       <div class="col mr-2">
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $item['title'] ?></div>
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                          Earnings (Monthly)</div>
+                          Revisa tu Orden</div>
                         
                       </div>
                       <div class="col-auto shadow">
