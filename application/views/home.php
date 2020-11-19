38dff08cc6979447dca3e0fd1ba6f8718ece5554
Veloz-X <viralveloz@gmail.com>
2020-11-19 16:42:54 -0500

    null
    null

--- application/views/home.php
+++ application/views/home.php
@@ -272,7 +272,7 @@ $giftcard = array(
               <?php foreach ($servicio as $item) : ?>
 
               <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card border-left-success shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
