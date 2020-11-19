fb0aec817adc02498867a132a13770d9205e120c
Veloz-X <viralveloz@gmail.com>
2020-11-19 15:35:02 -0500

    Prueba 4
    Prueba 4

--- application/views/home.php
+++ application/views/home.php
@@ -105,7 +105,7 @@ $giftcard = array(
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
   <!-- Popper JS -->
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
   <!-- Latest compiled JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -344,13 +344,16 @@ $giftcard = array(
                         <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                       </div>
                       <div class="col-auto">
-                        <i class="fas fa-comments fa-2x text-gray-300"></i>
+                      <img class="p-1" src="https://www.storeandgamers.com/uploads/logo/logo_5ed1c11998a10.png" width="150">
+                        <!-- <i class="fas fa-comments fa-2x text-gray-300"></i> -->
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+
+
             <?php foreach ($servicio as $item) : ?>
               <!-- gg -->
               <a href="<?= $item['url'] ?>" class="text-decoration:none">
