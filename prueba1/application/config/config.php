dde748ed6523240af33de21278172f5f296d0020
Veloz-X <viralveloz@gmail.com>
2020-11-18 21:14:00 -0500

    Prueba3
    Prueba3

--- prueba1/application/config/config.php
+++ prueba1/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://trans.storeandgamers.com/prueba1/';
+$config['base_url'] = 'https://trans.storeandgamers.com/prueba1/';
 
 /*
 |--------------------------------------------------------------------------
