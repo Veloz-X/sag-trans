bd8bc2ea4f30c6b32b930b58fd20555a8ffa59e8
Veloz-X <viralveloz@gmail.com>
2020-11-18 20:52:26 -0500

    v.1.64.b
    v.1.64.b

--- application/models/Search_net_model.php
+++ application/models/Search_net_model.php
@@ -8,15 +8,15 @@ class Search_net_model extends CI_Model
 		if($query != '')
 		{
 			$this->db->like('documento', $query);
-			$this->db->or_like('fecha_i', $query);
-			$this->db->or_like('fecha_f', $query);
-			$this->db->or_like('correo', $query);
-            $this->db->or_like('contra', $query);
-            $this->db->or_like('tipo', $query);
-            $this->db->or_like('estado', $query);
-            $this->db->or_like('nota', $query);
+			// $this->db->or_like('fecha_i', $query);
+			// $this->db->or_like('fecha_f', $query);
+			// $this->db->or_like('correo', $query);
+            // $this->db->or_like('contra', $query);
+            // $this->db->or_like('tipo', $query);
+            // $this->db->or_like('estado', $query);
+            // $this->db->or_like('nota', $query);
 		}
-		$this->db->order_by('fecha_i', 'DESC');
+		$this->db->order_by('documento', 'DESC');
 		return $this->db->get();
 	}
 }
