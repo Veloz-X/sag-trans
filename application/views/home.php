df84021f22816a49f0d0c3b9c8dab61a4faab2a9
Veloz-X <viralveloz@gmail.com>
2020-11-25 17:33:10 -0500

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -109,7 +109,7 @@ $giftcard = array(
 
   <!-- Latest compiled JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <link href="https://http://transferencia.storeandgamers.com/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://transferencia.storeandgamers.com/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
