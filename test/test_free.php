211a244d2a990c2b694390a18a196904f3c71cb7
Veloz-X <viralveloz@gmail.com>
2020-11-25 08:15:23 -0500

    null
    mull

--- test/test_free.php
+++ test/test_free.php
@@ -71,7 +71,26 @@
     </tr>
     <hr>
     <hr>
+    <div class="col-xl-3 col-md-6 mb-4">
+                <a href="<?= $item['url'] ?>" class="text-decoration:none">
+                  <div class="card border-left-success shadow h-100 py-1">
+                    <div class="card-body">
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto shadow">
+                          <img class="" style="border-radius: 8px; float:left;" width="72" height="72" src="<?= $item['icono'] ?>">
+                        </div>
+                        <h5>&nbsp; &nbsp;</h5>
+                        <div class="col mr-2">
 
+                          <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $item['title'] ?></div>
+                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            Revisa tu Orden</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
     
 </body>
 </html>
