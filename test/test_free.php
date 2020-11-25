9b45f8f5777d40fd88a801a94dccb6391b081eb7
Veloz-X <viralveloz@gmail.com>
2020-11-25 17:30:29 -0500

    Update test_free.php

--- test/test_free.php
+++ test/test_free.php
@@ -17,7 +17,7 @@
 
   <!-- Latest compiled JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://http://transferencia.storeandgamers.com/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
